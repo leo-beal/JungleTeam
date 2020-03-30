5b0667e842ed9009a1d81d5c1ec2fd158c2503db
Jacob <jacobmanna@hotmail.com>
2020-03-29 22:40:29 -0600

    Added sample torch and pedestal puzzle setup

---
+++ JungleAdventure/Assets/Custom C# Scripts/Tool_Torch/Fire.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    Flicker flicker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flicker = GetComponent<Flicker>();
+        flicker.enabled = this.enabled;
+    }
+
+    private void OnDisable()
+    {
+        if (flicker != null)
+            flicker.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        if (flicker != null)
+            flicker.enabled = true;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!this.enabled)
+            return;
+
+        if (collision.collider.CompareTag("Flamable"))
+        {
+            collision.collider.enabled = false;
+            collision.gameObject.SetActive(false);
+        }
+    }
+}
